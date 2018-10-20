187cd728ef0d515cbb40848d79ca0ea6a02d60c4
Khalab <hoomanak1@gmail.com>
2018-10-20 14:35:01 -0700

    update and cleaning directories

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,34 +1,39 @@
-#hello
-
+# First we'll import the os module
+# This will allow us to create file paths across operating systems
 import os
-import csv
 
+# Module for reading CSV files
+import csv
 
 csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
 
+with open(csvpath, newline='') as csvfile:
 
-with open(csvpath, newline = '') as csvfile:
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
 
-     csvreader = csv.reader(csvfile, delimiter =',')
-     #print(csvreader)
-     csv_header = next(csvreader)
-     #print(f"CSV Header: {csv_header}")
+    #print(csvreader)
 
-x = 0
-total = 0
-maxvalue = 0
-minvalue = 0
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
 
-     for row in csvreader:
-         x = x + 1
-         total = total + int(row[1])
+    total = 0
+    x = 0
+    maxvalue = 0
+    minvalue =0
 
-         if int(row[1]) > maxvalue:
-            maxvalue = int(row[1])
-            d = str(row[0])
-         elif int(row[1]) < minvalue:
-            minvalue = int(row[1])  
-            dd = str(row[0])
+    # Read each row of data after the header
+    for row in csvreader:
+        #print(row)
+        x += 1
+        total = total + int(row[1])
+        if int(row[1]) > maxvalue:
+           maxvalue = int(row[1])
+           d = str(row[0])
+        elif int(row[1]) < minvalue:
+           minvalue = int(row[1])  
+           dd = str(row[0])
 
 print("Financial Analysis")
 print("--------------------------------------")
@@ -38,5 +43,3 @@ print(f"Total: {total}")
 print(f"Greatest Increase in Profits: {d} ({maxvalue})")
 print(f"Greatest Decrease in Profits: {dd} ({minvalue})")
 
-
-        
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,61 +1,58 @@
 #election analysis
 
+# First we'll import the os module
+# This will allow us to create file paths across operating systems
 import os
-import csv
 
+# Module for reading CSV files
+import csv
 
 csvpath = os.path.join('..', 'Resources', 'election_data.csv')
 
+with open(csvpath, newline='') as csvfile:
 
-with open(csvpath, newline = '') as csvfile:
+    #CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
 
-     csvreader = csv.reader(csvfile, delimiter =',')
-     print(csvreader)
-     csv_header = next(csvreader)
-     print(f"CSV Header: {csv_header}")
+    #print(csvreader)
 
-x=0
-total1 = 0
-total2 = 0
-total3 = 0
-total4 = 0
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
 
-
+    x=0
+    total1 = 0
+    total2 = 0
+    total3 = 0
+    total4 = 0
     for row in csvreader:
         x = x + 1
-        
-
         if str(row[2]) == "Khan":
-           total1 = total1 + 1
-         
-        elif str(row[2}) == "Correy":
-           total2 = total2 + 1      
-
+            total1 = total1 + 1         
+        elif str(row[2]) == "Correy":
+            total2 = total2 + 1      
         elif str(row[2]) == "Li":
-           total3 = total3 + 1
-
-        elif str(row[2]) == "O'Tooley"
+            total3 = total3 + 1
+        elif str(row[2]) == "O'Tooley":
            total4 = total4 + 1
        
-t1 = round((total1/x)*100)
-t2 = round((total2/x)*100)
-t3 = round((total3/x)*100)
-t4 = round((total4/x)*100)
+    t1 = round((total1/x)*100)
+    t2 = round((total2/x)*100)
+    t3 = round((total3/x)*100)
+    t4 = round((total4/x)*100)
 
+    if t1 > t2:
+        a = "Khan"
+    elif t1 < t2:
+        a = "Correy"
+        t1 = t2
 
-         
-         if t1 > t2:
-           a = "Khan"
-         elif t1 < t2:
-           a = "Correy"
-           t1 =t2
+    if t1 < t3:
+        t1 =t3
+        a = "Li"
 
-         if t1 < t3:
-           t1 =t3
-           a = "Li"
-         
-         if t1 < t4
-           a = "O'Tooley"
+    if t1 < t4:
+        a = "O'Tooley"
 
 print("Election Results")
 print("--------------------------------------")
@@ -67,6 +64,6 @@ print(f"Li:  {t3}%   ({total3})")
 print(f"O'Tooley:  {t4}%  ({total4})")
 
 print("--------------------------------------")
-print(f"Winner: {a})
+print(f"Winner: {a}")
 print("--------------------------------------")
         
